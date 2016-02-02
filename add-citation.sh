7153705f8af208101e945395f227d7bb44a31575
Atte Keinänen <atte.keinanen@gmail.com>
2016-02-02 16:12:30 +0200

    Add refs über-easily from Google Scholar

---
+++ add-citation.sh
@@ -0,0 +1,7 @@
+set -uo pipefail
+search_query=$1
+
+printf "Fetching BibTeX entry from Google Scholar and adding it in the end of 'lahteet.bib' file... \n"
+output="\n$(./scholar.py -c 1 --phrase "$1" --citation bt 2> /dev/null)"
+echo "$output" >> lahteet.bib
+echo "$output"
