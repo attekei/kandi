aad20831c993d00ca042885104ac75406b6fe7ae
Atte Keinänen <atte.keinanen@gmail.com>
2016-02-07 23:19:50 +0200

    Add initial introduction, glossary etc

--- add-citation.sh
+++ add-citation.sh
@@ -3,5 +3,5 @@ search_query=$1
 
 printf "Fetching BibTeX entry from Google Scholar and adding it in the end of 'lahteet.bib' file... \n"
 output="\n$(./scholar.py -c 1 --phrase "$1" --citation bt 2> /dev/null)"
-echo "$output" >> lahteet.bib
+echo "$output" >> sources.bib
 echo "$output"
