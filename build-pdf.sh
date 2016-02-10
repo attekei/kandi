6f415855ff7419b470614d461b291fad38b81056
Atte Keinänen <atte.keinanen@gmail.com>
2016-02-10 21:41:58 +0200

    We have seen major advancements

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,9 +1,9 @@
 set -uo pipefail
-main_tex_file=$1
+document_name=$1
 
 # Show only output from bibtex and last last pdflatex run
 # I hate to get excessing amount of output when I run my beautiful scripts ;)
-pdflatex -interaction=nonstopmode $main_tex_file > /dev/null
-bibtex $main_tex_file
-pdflatex -interaction=nonstopmode $main_tex_file > /dev/null
-pdflatex -interaction=nonstopmode -halt-on-error $main_tex_file
+pdflatex -interaction=nonstopmode $document_name, > /dev/null
+bibtex $document_name
+pdflatex -interaction=nonstopmode $document_name > /dev/null
+pdflatex -interaction=nonstopmode -halt-on-error $document_name
