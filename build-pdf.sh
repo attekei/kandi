d66588832638ff355d2acf2022fc7ee7769bbcce
Atte Keinänen <atte.keinanen@gmail.com>
2017-11-25 17:31:40 +0200

    Continuing to work on my thesis after a 1,5 year break lol

--- build-pdf.sh
+++ build-pdf.sh
@@ -3,7 +3,7 @@ document_name=$1
 
 # Show only output from bibtex and last last pdflatex run
 # I hate to get excessing amount of output when I run my beautiful scripts ;)
-pdflatex -interaction=nonstopmode $document_name, > /dev/null
+pdflatex -shell-escape -interaction=nonstopmode $document_name > /dev/null
 bibtex $document_name
-pdflatex -interaction=nonstopmode $document_name > /dev/null
-pdflatex -interaction=nonstopmode -halt-on-error $document_name
+pdflatex -shell-escape -interaction=nonstopmode $document_name > /dev/null
+pdflatex -shell-escape -interaction=nonstopmode -halt-on-error $document_name
